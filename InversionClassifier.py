9c68b56a338524ad097577cd35f2498301a2f21c
TwoKneeZ <AKZacharatos@gmail.com>
2024-11-29 01:17:40 -0500

    Added a Left and Right Inverse Classifier

--- InversionClassifier.py
+++ InversionClassifier.py
@@ -12,7 +12,7 @@ class InversionClassifier(nn.Module):
         super(InversionClassifier, self).__init__()
         
         self.activateFn = nn.LeakyReLU(negative_slope=.01)
-        self.dropout = nn.Dropout(p=0.4)
+        self.dropout = nn.Dropout(p=0.25)
              
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
@@ -28,6 +28,9 @@ class InversionClassifier(nn.Module):
         self.bn6 = nn.BatchNorm2d(256)
         self.conv7 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
         self.bn7 = nn.BatchNorm2d(512)
+        # self.conv8 = nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1)
+        # self.bn8 = nn.BatchNorm2d(1024)
+        
         
         self.pool = nn.MaxPool2d(2, 2)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
@@ -71,6 +74,11 @@ class InversionClassifier(nn.Module):
         x = self.pool(x)
         x = self.bn7(x)
         x = self.activateFn(x)
+        
+        # x = self.conv8(x)
+        # x = self.pool(x)
+        # x = self.bn8(x)
+        # x = self.activateFn(x)
         
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
